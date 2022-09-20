26ed6c77cebd23f8146237afe66544f9b46384df
SutharShantanu <shantanusut2000@Gmail.com>
2022-09-20 18:41:58 +0530

    code fix

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
+// Check if a number is a prime or not !
 
-
-let num=17;
-for(let i=1;i<=num;i++){
-    if(num%i==2){
+let num = 17, factor = 0;
+for (let i = 1; i <= num; i++) {
+    if (num % i == 0) {
         factor++;
     }
 }
-
-if(factor==2){
+if (factor == 2) {
     console.log("prime")
-} else{
+} else {
     console.log("not prime")
 }
